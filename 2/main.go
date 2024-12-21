8e3264721291368840ebff25125f634d37b8b61b
Fabrício Soares <eu@fabriciosoares.com>
2024-12-21 18:01:15 -0300

    2

---
+++ 2/main.go
@@ -0,0 +1,21 @@
+package main
+
+// global
+const a = "Hello, World"
+
+var(
+ b bool
+ c int
+ d string
+ e float64
+)
+
+func main(){
+  // local
+  var a string = "Hello, World" // explicit type
+  var b := "Hello, World" // implicit type
+  var c:= "Hello, World" // short declaration
+
+  println(a)
+	println(e)
+}
